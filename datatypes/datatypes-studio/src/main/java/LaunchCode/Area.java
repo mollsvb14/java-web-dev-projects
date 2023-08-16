656a28a85a2c2f35a51020f9ad5846f50546af63
Mollsilita <mollsilita@loaner-mbp.lan>
2023-08-16 14:34:50 -0500

    class one studio git push

---
+++ datatypes/datatypes-studio/src/main/java/LaunchCode/Area.java
@@ -0,0 +1,14 @@
+package LaunchCode;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        System.out.println("Enter a radius:");
+        double radius=input.nextDouble();
+        double area= Circle.getArea(radius);
+         System.out.println(area);
+
+
+    }
+
+}
